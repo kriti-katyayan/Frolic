462f6685ea1c5ca963ebc2df8239d46dea7a1d10
kritikatyayan <kriti.pune@gmail.com>
2020-06-30 21:19:51 +0530

    Kriti_Login Screen UI

---
+++ app/src/main/java/com/tiaa/tiaaprepupproject/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.tiaa.tiaaprepupproject;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    ImageView backBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        backBtn=findViewById(R.id.backBtn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); //it will take us back to the main activity
+            }
+        });
+
+    }
+}
