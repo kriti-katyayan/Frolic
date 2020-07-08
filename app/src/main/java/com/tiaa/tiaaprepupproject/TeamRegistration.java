7f871b7ec2f3c934012131be70a41291b0b41696
kritikatyayan <kriti.pune@gmail.com>
2020-07-09 00:35:54 +0530

    Kriti_PlayerRegistration Layout,Team Reg layout, event desc layout

---
+++ app/src/main/java/com/tiaa/tiaaprepupproject/TeamRegistration.java
@@ -0,0 +1,14 @@
+package com.tiaa.tiaaprepupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TeamRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_registration);
+    }
+}
